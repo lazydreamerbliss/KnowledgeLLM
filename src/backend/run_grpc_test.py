276b9f1d6eed6f645bb8a13750a1aeab5d3e94f3
chengjia <michaelxuchj@gmail.com>
2024-02-23 11:52:35 +0800

    ?

--- src/backend/run_grpc_test.py
+++ src/backend/run_grpc_test.py
@@ -4,11 +4,15 @@ from pathlib import Path
 
 from tests.grpc_client_test import GrpcClientForServerTest
 
+from backend.constants.env import IS_LINUX, IS_OSX
+
 
 def run_test_server_in_new_process() -> int:
     server_file: str = f'{Path(__file__).parent}/run_grpc_server.py'
-    process = subprocess.Popen(['nohup', '/Users/chengjia/anaconda3/envs/llm_general/bin/python', server_file])
-    # process = subprocess.Popen(['nohup', '/home/chengjia/anaconda3/envs/llm_general/bin/python', server_file])
+    if IS_OSX:
+        process = subprocess.Popen(['nohup', '/Users/chengjia/anaconda3/envs/llm_general/bin/python', server_file])
+    elif IS_LINUX:
+        process = subprocess.Popen(['nohup', '/home/chengjia/anaconda3/envs/llm_general/bin/python', server_file])
     return process.pid
 
 
@@ -16,8 +20,7 @@ if __name__ == '__main__':
     pid: int = run_test_server_in_new_process()
     print(f'gRPC server started with pid: {pid}')
 
-    # Sleep for some time to let the server start, otherwise "Failed to
-    # connect to remote host: Connection refuse" might occur
+    # Sleep for some time to let the server start, otherwise "Failed to connect to remote host: Connection refuse" might occur
     time.sleep(3)
 
     try:
