724e4df2b00b343d966b618d9f4df1841c4c5ccb
chengjia <michaelxuchj@gmail.com>
2024-02-27 22:32:27 +0800

    remove grpc files from tracking

--- src/launcher/server/grpc/backend_pb.d.ts
+++
@@ -1,9 +0,0 @@
-// package: 
-// file: backend.proto
-
-/* tslint:disable */
-/* eslint-disable */
-
-import * as jspb from "google-protobuf";
-import * as obj_basic_pb from "./obj_basic_pb";
-import * as obj_shared_pb from "./obj_shared_pb";
